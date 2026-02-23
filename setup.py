8197ea086d37f3815637f94b1709985409b70b3f
Xiao, Shaoning <xiaoshaoning@foxmail.com>
2026-02-24 02:05:48 +0800

    Add GPLv3 license and update documentation
    - Add LICENSE file with GNU General Public License v3.0
    - Update README.md to reflect GPLv3 license
    - Update setup.py classifier from MIT to GPLv3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
